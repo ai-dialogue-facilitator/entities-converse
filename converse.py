fcbe8fb45cb736a6ba975f6623ed95a592c37adb
alxfed <alxfed@gmail.com>
2023-12-28 07:12:01 -0600

    Structure

--- converse.py
+++ converse.py
@@ -5,32 +5,35 @@
 This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
-from conversation import Conversation, Participant, Utterance
+from conversation import Conversation, Participant
 
 
 def main():
-    human = Participant(name="human",
-                        role="user",
-                        instructions="")
+    # human = Participant(name="human",
+    #                     role="user",
+    #                     instructions="")
+    #
+    # facilitator_instructions = ("This is a facilitator"
+    #                             " instructions.")
+    # facilitator = Participant(name="facilitator",
+    #                         role="assistant",
+    #                         instructions=facilitator_instructions)
+    #
+    # protagonist = Participant(name="protagonist",
+    #                           role="assistant",
+    #                           instructions="")
+    # antagonist = Participant(name="antagonist",
+    #                         role="assistant",
+    #                         instructions="")
+    #
+    # participant_list = [human, facilitator, protagonist, antagonist]
+    # kwa = {
+    #     "participants": participant_list
+    # }
+    conversation = Conversation()
+    print("End of conversation")
 
-    facilitator_instructions = ("This is a facilitator"
-                                " instructions.")
-    facilitator = Participant(name="facilitator",
-                            role="assistant",
-                            instructions=facilitator_instructions)
-
-    protagonist = Participant(name="protagonist",
-                              role="assistant",
-                              instructions="")
-    antagonist = Participant(name="antagonist",
-                            role="assistant",
-                            instructions="")
-
-    participant_list = [human, facilitator, protagonist, antagonist]
-    conversation = Conversation(participants=participant_list)
-    pass
 
 
 if __name__ == "__main__":
     main()
-    print("ok")
--- src/conversation.py
+++ src/conversation.py
@@ -5,7 +5,7 @@
 This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
-from typing import List
+from typing import List, Dict
 from dataclasses import dataclass, field, asdict
 import json
 import jsonlines
@@ -16,6 +16,8 @@ class Participant:
     name:           str = field(default="")
     role:           str = field(default="")
     instructions:   str = field(default="")
+    id:             str = field(default="")
+    thread_id:      str = field(default="")
 
     def to_dict(self):
         return asdict(self)
@@ -30,27 +32,38 @@ class Utterance:
         return asdict(self)
 
 
-class Conversation:
+class Conversation():
     """Represents a conversation."""
-    participants: List = field(default_factory=list)
-    record: List = field(default_factory=list)
-    threads: List = field(default_factory=list)
+    participants:   List = field(default_factory=list)
+    record:         List = field(default_factory=list)
+    threads:        List = field(default_factory=list)
     agenda: dict = field(default_factory=dict)
+
+    def __init__(self, **kwargs):
+        self._load_agenda()
+        self._load_record()
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        super(Conversation, self).__init__()
 
     def add_utterance(self, utterance: Utterance):
         self.record.append(utterance)
 
     def _save_utterance(self, utterance: Utterance):
         dict = utterance.to_dict()
-        with jsonlines.open('record.jsonl', 'w') as writer:
+        with jsonlines.open('../record.jsonl', 'w') as writer:
             writer.write(utterance.to_dict())
 
+    def _load_participants(self):
+        with open('../participants/list.json', 'r') as file:
+            self.participants = json.load(file)
+
     def _load_record(self):
-        with jsonlines.open('record.jsonl', 'r') as reader:
+        with jsonlines.open('../record.jsonl', 'r') as reader:
             self.record = [Utterance(**line) for line in reader.iter()]
 
     def _load_agenda(self):
-        with open('agenda.json', 'r') as file:
+        with open('../agenda.json', 'r') as file:
             self.agenda = json.load(file)
 
     def next_statement(self):
